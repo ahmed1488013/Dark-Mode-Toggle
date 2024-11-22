fdf0776dc64ca83c4bc0902277d7cef354d0f092
ahmed1488013 <ah625634@gmail.com>
2024-11-22 14:33:13 +0300

    Dark Mode Toggle
    Dark Mode Toggle

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggleButton = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme');
+  
+    if (currentTheme === 'dark') {
+      document.body.classList.add('dark-mode');
+    }
+  
+    themeToggleButton.addEventListener('click', () => {
+      document.body.classList.toggle('dark-mode');
+      const theme = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
+      localStorage.setItem('theme', theme);
+    });
+  });
+  
